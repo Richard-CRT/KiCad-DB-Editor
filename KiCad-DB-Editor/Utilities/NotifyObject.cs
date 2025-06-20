39eb6ad9e5da870031f3335beda891cd93aa7a3f
Richard-CRT <richard-crt@users.noreply.github.com>
2025-06-20 11:48:21 +0100

    Move NotifyObject to common file for Model and ViewModel to use

--- KiCad-DB-Editor/Utilities/NotifyObject.cs
+++ KiCad-DB-Editor/Utilities/NotifyObject.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KiCad_DB_Editor.Model
+namespace KiCad_DB_Editor
 {
     public class NotifyObject : INotifyPropertyChanged
     {

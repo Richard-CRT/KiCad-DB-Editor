fa1d93763b347911a4df69e64878d5bb26746998
Richard-CRT <richard-crt@users.noreply.github.com>
2025-10-03 09:58:58 +0100

    Add @ to safe category & parameter names

--- KiCad-DB-Editor/Utilities/Util.cs
+++ KiCad-DB-Editor/Utilities/Util.cs
@@ -28,11 +28,11 @@ namespace KiCad_DB_Editor.Utilities
             "footprint",
         };
 
-        // Definitely no quotes, I don't want to have to detail with DB sanitisation
+        // Definitely no quotes, I don't want to have to deal with DB sanitisation
         // No slashes, forward or back, as could be used as a delimeter / escape character
         // No | as used for a visual splitter for the KiCad categories
-        public static HashSet<char> SafeCategoryCharacters = new HashSet<char>("abcdefghjiklmnopqrstuvwxyz0123456789_-& ");
-        public static HashSet<char> SafeParameterCharacters = new HashSet<char>("abcdefghjiklmnopqrstuvwxyz0123456789_-& ");
+        public static HashSet<char> SafeCategoryCharacters = new HashSet<char>("abcdefghjiklmnopqrstuvwxyz0123456789_-&@ ");
+        public static HashSet<char> SafeParameterCharacters = new HashSet<char>("abcdefghjiklmnopqrstuvwxyz0123456789_-&@ ");
 
         public static Dictionary<byte, char> Base32EncodeBook = new()
         {

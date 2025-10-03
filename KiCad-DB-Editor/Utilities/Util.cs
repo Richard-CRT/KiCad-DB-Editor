68c9cdb11acb1e74d1da8543c2be02ad21aad0b4
Richard-CRT <richard-crt@users.noreply.github.com>
2025-10-03 10:59:12 +0100

    Add parentheses as safe category and parameter characters

--- KiCad-DB-Editor/Utilities/Util.cs
+++ KiCad-DB-Editor/Utilities/Util.cs
@@ -31,8 +31,8 @@ namespace KiCad_DB_Editor.Utilities
         // Definitely no quotes, I don't want to have to deal with DB sanitisation
         // No slashes, forward or back, as could be used as a delimeter / escape character
         // No | as used for a visual splitter for the KiCad categories
-        public static HashSet<char> SafeCategoryCharacters = new HashSet<char>("abcdefghjiklmnopqrstuvwxyz0123456789_-&@ ");
-        public static HashSet<char> SafeParameterCharacters = new HashSet<char>("abcdefghjiklmnopqrstuvwxyz0123456789_-&@ ");
+        public static HashSet<char> SafeCategoryCharacters = new HashSet<char>("abcdefghjiklmnopqrstuvwxyz0123456789_-&@() ");
+        public static HashSet<char> SafeParameterCharacters = new HashSet<char>("abcdefghjiklmnopqrstuvwxyz0123456789_-&@() ");
 
         public static Dictionary<byte, char> Base32EncodeBook = new()
         {

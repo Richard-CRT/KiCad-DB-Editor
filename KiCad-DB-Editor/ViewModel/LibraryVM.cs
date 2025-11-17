a644607878541a26aba9bbd59b2ac4bd687fcd00
Richard Carter <Richard-CRT@users.noreply.github.com>
2025-11-17 20:52:12 +0000

    Switch updating Part UID scheme to a command rather than a direct binding and defaults to existing scheme (closes #30)

--- KiCad-DB-Editor/ViewModel/LibraryVM.cs
+++ KiCad-DB-Editor/ViewModel/LibraryVM.cs
@@ -77,6 +77,20 @@ namespace KiCad_DB_Editor.ViewModel
             }
         }
 
+        private string _newPartUIDScheme = "";
+        public string NewPartUIDScheme
+        {
+            get { return _newPartUIDScheme; }
+            set
+            {
+                if (_newPartUIDScheme != value)
+                {
+                    _newPartUIDScheme = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
         private string _newUniversalParameterName = "";
         public string NewUniversalParameterName
         {
@@ -242,6 +256,8 @@ namespace KiCad_DB_Editor.ViewModel
             // Link model
             Library = library;
 
+            NewPartUIDScheme = library.PartUIDScheme;
+
             Library.TopLevelCategories.CollectionChanged += Library_TopLevelCategories_CollectionChanged;
             TopLevelCategoryVMs = new(library.TopLevelCategories.Select(c => new CategoryVM(c)));
             Debug.Assert(_topLevelCategoryVMs is not null);
@@ -256,6 +272,8 @@ namespace KiCad_DB_Editor.ViewModel
             NewTopLevelCategoryCommand = new BasicCommand(NewTopLevelCategoryCommandExecuted, NewTopLevelCategoryCommandCanExecute);
             NewSubCategoryCommand = new BasicCommand(NewSubCategoryCommandExecuted, NewSubCategoryCommandCanExecute);
             DeleteCategoryCommand = new BasicCommand(DeleteCategoryCommandExecuted, DeleteCategoryCommandCanExecute);
+
+            UpdatePartUIDSchemeCommand = new BasicCommand(UpdatePartUIDSchemeCommandExecuted, UpdatePartUIDSchemeCommandCanExecute);
 
             NewUniversalParameterCommand = new BasicCommand(NewUniversalParameterCommandExecuted, NewUniversalParameterCommandCanExecute);
             RenameUniversalParameterCommand = new BasicCommand(RenameUniversalParameterCommandExecuted, RenameUniversalParameterCommandCanExecute);
@@ -380,6 +398,7 @@ namespace KiCad_DB_Editor.ViewModel
         public IBasicCommand NewTopLevelCategoryCommand { get; }
         public IBasicCommand NewSubCategoryCommand { get; }
         public IBasicCommand DeleteCategoryCommand { get; }
+        public IBasicCommand UpdatePartUIDSchemeCommand { get; }
         public IBasicCommand NewUniversalParameterCommand { get; }
         public IBasicCommand RenameUniversalParameterCommand { get; }
         public IBasicCommand DeleteUniversalParameterCommand { get; }
@@ -462,6 +481,16 @@ namespace KiCad_DB_Editor.ViewModel
                     walkCategory = walkCategory.ParentCategory;
                 }
             }
+        }
+
+        private bool UpdatePartUIDSchemeCommandCanExecute(object? parameter)
+        {
+            return NewPartUIDScheme.Count(c => c == '#') == Util.PartUIDSchemeNumberOfWildcards;
+        }
+
+        private void UpdatePartUIDSchemeCommandExecuted(object? parameter)
+        {
+            Library.PartUIDScheme = NewPartUIDScheme;
         }
 
         private bool NewUniversalParameterCommandCanExecute(object? parameter)
